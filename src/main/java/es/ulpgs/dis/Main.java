52607960ffd3d48c4063667b0118389f1dbe0b02
Joel Santana Aleman <26joelaleman@gmail.com>
2024-10-29 13:13:24 +0000

    class finish

--- src/main/java/es/ulpgs/dis/Main.java
+++ src/main/java/es/ulpgs/dis/Main.java
@@ -1,4 +1,10 @@
 package es.ulpgs.dis;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        Person joel = new Person("Joel", LocalDate.of(2003, 7,26));
+        System.out.println(joel);
+    }
 }
